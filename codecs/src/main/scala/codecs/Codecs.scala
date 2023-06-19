df1428e8b238f1b2da454c25a58424a1c89a8b9f
naosense <pingao777@gmail.com>
2023-06-19 13:58:18 +0800

    format

--- codecs/src/main/scala/codecs/Codecs.scala
+++ codecs/src/main/scala/codecs/Codecs.scala
@@ -305,11 +305,11 @@ import Util.*
   println(parseJson(personStr).flatMap(_.decodeAs[Person]))
 
   val maybeJsonString = parseJson(""" "foo" """)
-  val maybeJsonObj    = parseJson(""" { "name": "Alice", "age": 42 } """)
-  val maybeJsonObj2   = parseJson(""" { "name": "Alice", "age": "42" } """)
+  val maybeJsonObj = parseJson(""" { "name": "Alice", "age": 42 } """)
+  val maybeJsonObj2 = parseJson(""" { "name": "Alice", "age": "42" } """)
   // Uncomment the following lines as you progress in the assignment
-   println(maybeJsonString.flatMap(_.decodeAs[Int]))
-   println(maybeJsonString.flatMap(_.decodeAs[String]))
-   println(maybeJsonObj.flatMap(_.decodeAs[Person]))
-   println(maybeJsonObj2.flatMap(_.decodeAs[Person]))
-   println(renderJson(Person("Bob", 66)))
+  println(maybeJsonString.flatMap(_.decodeAs[Int]))
+  println(maybeJsonString.flatMap(_.decodeAs[String]))
+  println(maybeJsonObj.flatMap(_.decodeAs[Person]))
+  println(maybeJsonObj2.flatMap(_.decodeAs[Person]))
+  println(renderJson(Person("Bob", 66)))
